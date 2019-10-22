195faa67c912af1697597f1d06fb1311d463611a
migudmigu <migud.carvalho@gmail.com>
2019-10-22 18:22:36 +0100

    location

--- src/agents/Client.java
+++ src/agents/Client.java
@@ -7,27 +7,69 @@ import jade.domain.FIPAException;
 import jade.domain.FIPANames;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAAgentManagement.Property;
 import jade.domain.FIPAAgentManagement.SearchConstraints;
 import jade.util.leap.Iterator;
+import jade.proto.SubscriptionInitiator;
+import jade.lang.acl.ACLMessage;
 
 public class Client extends Agent {
 
   protected void setup() {
-  	// Search for services of type "weather-forecast"
-  	System.out.println("Agent "+getLocalName()+" searching for services of type \"tech-repairs\"");
-  	try {
-  		// Build the description used as template for the search
+
+	String serviceType = "tech-repairs";
+		  
+  		// Build the description used as template for the subscribe
   		DFAgentDescription template = new DFAgentDescription();
   		ServiceDescription templateSd = new ServiceDescription();
-  		templateSd.setType("tech-repairs");
+		//templateSd.setName("TechRepairs");
+  		templateSd.setType(serviceType);
   		template.addServices(templateSd);
   		
   		SearchConstraints sc = new SearchConstraints();
-  		// We want to receive 10 results at most
-  		sc.setMaxResults(new Long(10));
+  		// We want to receive 100 results at most
+  		sc.setMaxResults(new Long(100));
+
+		// Subscribe
+
+  		System.out.println("Agent "+getLocalName()+" subscribing for services of type \"" + serviceType + "\"");
+
+		addBehaviour(new SubscriptionInitiator(this, DFService.createSubscriptionMessage(this, getDefaultDF(), template, sc)) {
+			protected void handleInform(ACLMessage inform) {
+  			System.out.println("Agent "+getLocalName()+": Notification received from DF");
+  			try {
+					DFAgentDescription[] results = DFService.decodeNotification(inform.getContent());
+		  		if (results.length > 0) {
+		  			for (int i = 0; i < results.length; ++i) {
+		  				DFAgentDescription dfd = results[i];
+		  				AID provider = dfd.getName();
+		  				// The same agent may provide several services; we are only interested
+		  				// in the tech-repairs one
+		  				Iterator it = dfd.getAllServices();
+		  				while (it.hasNext()) {
+		  					ServiceDescription sd = (ServiceDescription) it.next();
+		  					if (sd.getType().equals(serviceType)) {
+	  							System.out.println(serviceType + " service for found:");
+		  						System.out.println("- Service \""+sd.getName()+"\" provided by agent "+provider.getName());
+		  					}
+		  				}
+		  			}
+		  		}	
+	  			System.out.println();
+		  	}
+		  	catch (FIPAException fe) {
+		  		fe.printStackTrace();
+		  	}
+			}
+		} );
   		
-  		DFAgentDescription[] results = DFService.search(this, template, sc);
-  		if (results.length > 0) {
+		  /*
+  		DFAgentDescription[] results = DFService.search(this, template); // sc
+
+		  //	Blocks for 5 seconds
+  		//DFAgentDescription[] results = DFService.searchUntilFound(this, this.getName(), template, sc, 1000*5);
+		  
+  		if (results != null && results.length > 0) {
   			System.out.println("Agent "+getLocalName()+" found the following tech-repairs services:");
   			for (int i = 0; i < results.length; ++i) {
   				DFAgentDescription dfd = results[i];
@@ -42,14 +84,12 @@ public class Client extends Agent {
   					}
   				}
   			}
-  		}	
+  		}
   		else {
   			System.out.println("Agent "+getLocalName()+" did not find any tech-repairs service");
   		}
-  	}
-  	catch (FIPAException fe) {
-  		fe.printStackTrace();
-  	}
+	 */ 
+  	
   } 
 }
 
--- src/agents/Technician.java
+++ src/agents/Technician.java
@@ -9,16 +9,38 @@ import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.FIPAAgentManagement.Property;
 
+/**
+*		args[] -> name, x, y
+*/
 public class Technician extends Agent {
 
+	//private Random rand;	//	for random location
+
+	//	We need to create a map and decide a range. tipo: 100x100
+	//private Pair<x,y> location;	Use pairs or x and y?
+
+	private int x;
+	private int y;
+
   protected void setup() {
-  	String serviceName = "unknown";
+  	String serviceName = "TechRepairs";
   	
   	// Read the name of the service to register as an argument
   	Object[] args = getArguments();
-  	if (args != null && args.length > 0) {
-  		serviceName = (String) args[0];
-  	}
+  	if (args != null) {
+			switch(args.length){
+				case 2:	// Supose its x and y
+					x = tryParse(args[0]);
+					y = tryParse(args[1]);
+					break;
+				default:
+					System.out.println("Usage agent:xcoordinate:ycoordinate");
+					System.out.println("Creating agent with random coordinates.");
+  		}
+				
+		}
+		
+
   	
   	// Register the service
   	System.out.println("Agent "+getLocalName()+" registering service \""+serviceName+"\" of type \"tech-repairs\"");
@@ -30,10 +52,10 @@ public class Technician extends Agent {
   		sd.setName(serviceName);
   		sd.setType("tech-repairs");
   		// Agents that want to use this service need to "know" the tech-repairs-ontology
-  		// sd.addOntologies("tech-repairs-ontology");
+  		sd.addOntologies("tech-repairs-ontology");
   		// Agents that want to use this service need to "speak" the FIPA-SL language
-  		// sd.addLanguages(FIPANames.ContentLanguage.FIPA_SL);
-  		// sd.addProperties(new Property("country", "Italy"));
+  		sd.addLanguages(FIPANames.ContentLanguage.FIPA_SL);
+  		sd.addProperties(new Property("country", "Italy"));
   		dfd.addServices(sd);
   		
   		DFService.register(this, dfd);
@@ -58,5 +80,18 @@ public class Technician extends Agent {
 
     System.out.println("Technician-agent " + getLocalName() + " has terminated!");
   } 
+	
+	public static Integer tryParse(String text)
+	{
+		try {
+			return Integer.parseInt(text);
+		} catch (NumberFormatException e) {
+			System.out.println("Wrong coordinates format.");
+			System.out.println("Generating random coordinates.");
+			Random rand;
+			return rand.nextInt(101);
+  }
+}
+
 }
 
