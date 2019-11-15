2c1e10fa6b9def07b58fafc8cdfd2764de05330b
migudmigu <migud.carvalho@gmailcom>
2019-11-15 23:51:14 +0000

    Continued Technician and Company

--- src/agents/Company.java
+++ src/agents/Company.java
@@ -4,10 +4,13 @@ import java.util.HashMap;
 
 import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
 import jade.domain.DFService;
 import jade.domain.FIPAException;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
 import strategies.company.AllocStrategy;
 import strategies.company.AllocStrategy1;
 import strategies.company.AllocStrategy2;
@@ -20,7 +23,7 @@ import utils.Logger;
 public class Company extends Agent {
 
     private String serviceName = "Company_";
-    private String serviceType = "company";  
+    private String serviceType = "companyservice";  
 
     private HashMap<AID, String> technicians;
     private HashMap<AID, String> stations;
@@ -37,7 +40,7 @@ public class Company extends Agent {
     /**
      * € Technicians receive per Repair
      */
-    private double repairCommission;
+    public double repairCommission;
     
     /**
      * € Technicians receive per joining the Company
@@ -49,13 +52,11 @@ public class Company extends Agent {
      */
     private double contractTime;
 
-    // TODO: Decide which ones are public to stranger Technicians
-
     public Company(String companyName, TechDistributionStrategy techDistributionStrategy, PaymentStrategy paymentStrategyParam, AllocStrategy allocStrategyParam) {
         technicians = new HashMap<AID, String>();
         stations = new HashMap<AID, String>();
 
-        serviceName += companyName; 
+        serviceName += companyName;
         
         this.techDistributionStrategy = techDistributionStrategy;
         this.paymentStrategy = paymentStrategy; 
@@ -99,8 +100,43 @@ public class Company extends Agent {
             e.printStackTrace();
         }
         
+        addBehaviour(new SubscriptionListener(this, "company-subscription", technicians));
 
-        //this.addBehaviour();
+    }
+    
+    class SubscriptionListener extends CyclicBehaviour {
+        private static final long serialVersionUID = 9068977292715279066L;
 
-    } 
+        private final MessageTemplate mt;
+        private final String ontology;
+        private final HashMap<AID, String> agentsMap;
+
+        SubscriptionListener(Agent a, String ontology, HashMap<AID, String> agentsMap) {
+            super(a);
+            this.ontology = ontology;
+            this.agentsMap = agentsMap;
+
+            MessageTemplate subscribe = MessageTemplate.MatchPerformative(ACLMessage.SUBSCRIBE);
+            MessageTemplate unsubscribe = MessageTemplate.MatchPerformative(ACLMessage.CANCEL);
+            MessageTemplate tp = MessageTemplate.MatchOntology(ontology);
+            this.mt = MessageTemplate.and(MessageTemplate.or(subscribe, unsubscribe), tp);
+        }
+
+        @Override
+        public void action() {  // Deviamos por block() em vez de tar sempre a retornar null -> performance
+            ACLMessage message = receive(mt);
+            if (message == null) return;
+
+            if (message.getPerformative() == ACLMessage.SUBSCRIBE) {
+                this.agentsMap.put(message.getSender(), "");
+            } else {
+                this.agentsMap.remove(message.getSender());
+            }
+
+            message.createReply();
+            message.setPerformative(ACLMessage.CONFIRM);
+            message.setContent(salary + " ; " + contractBonus + " ; " + contractTime);
+            send(message);
+        }
+    }
 }
---
+++ src/agents/Technician.java
@@ -0,0 +1,106 @@
+package agents;
+
+import jade.core.AID;
+import jade.core.Agent;
+import jade.core.behaviours.OneShotBehaviour;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import utils.Logger;
+import utils.TimeBoard;
+
+public class Technician extends Agent {
+
+    TimeBoard timeBoard;
+    AID company;
+    AID newCompany;
+
+    public Technician(AID newCompany) {
+        this.newCompany = newCompany;
+        this.timeBoard = new TimeBoard();
+    }
+
+    protected void setup() {
+        timeBoard = new TimeBoard();
+
+        Logger.info(getLocalName(), "Setup Technician Agent");
+
+        addBehaviour(new SubscribeCompany());
+    }
+
+    protected void takeDown() {
+
+
+
+        Logger.warn(getLocalName(), "Technician Terminated!");
+    }
+
+    public TimeBoard getTimeBoard() {
+        return timeBoard;
+    }
+
+    public class SubscribeCompany extends OneShotBehaviour {
+
+        String ontology = "technician-subscription";
+        
+        @Override
+        public void action() {
+            MessageTemplate mt =  MessageTemplate.and(MessageTemplate.MatchPerformative(ACLMessage.CONFIRM), MessageTemplate.MatchOntology(ontology));
+            
+            ACLMessage message = new ACLMessage(ACLMessage.SUBSCRIBE);
+            message.setOntology(ontology);
+            message.addReceiver(newCompany);
+            send(message);
+            
+            ACLMessage responseMessage = null;
+
+            while(responseMessage == null) {
+                block();
+                responseMessage = receive(mt);
+            }
+
+            String responseContent = responseMessage.getContent();
+
+            // Change company
+
+            Logger.info(myAgent.getLocalName(),"Received subscription confirmation from company: " + message.getSender());
+
+            company = newCompany;
+
+            //contractTime = received contract time
+            //salary = salary received
+            //repaircomission = received
+            //contracttime
+        }
+
+    }
+    
+    public class UnsubscribeCompany extends OneShotBehaviour {
+
+        String ontology = "company-subscription";
+        
+        @Override
+        public void action() {
+            MessageTemplate mt =  MessageTemplate.and(MessageTemplate.MatchPerformative(ACLMessage.CONFIRM), MessageTemplate.MatchOntology(ontology));
+            
+            ACLMessage message = new ACLMessage(ACLMessage.SUBSCRIBE);
+            message.setOntology(ontology);
+            message.addReceiver(company);
+            send(message);
+
+            while(response == null) {
+                block();
+                response = receive();
+            }
+            ACLMessage responseMessage = blockingReceive(mt);
+
+            String responseContent = responseMessage.getContent();
+
+
+
+            Logger.info(myAgent.getLocalName(),"Received subscription confirmation from company: " + message.getSender());
+            
+        }
+
+    }
+
+}
