6785eb5b0e039ee95be7bed5f7857c1f3b1add77
Nuno Lopes <up201605337@fe.up.pt>
2019-11-18 03:20:33 +0000

    Company strategies

--- src/simulation/AIADWorld.java
+++ src/simulation/AIADWorld.java
@@ -2,6 +2,7 @@ package simulation;
 
 import strategies.SimpleCompanyStrategy;
 import strategies.SimpleTechnicianStrategy;
+import strategies.UmNomeParaMeter;
 import strategies.UniformClientStrategy;
 
 public class AIADWorld extends World {
@@ -9,23 +10,24 @@ public class AIADWorld extends World {
         numberDays = 100;
 
         T = 160;
-        technicians = new TechniciansDesc[] {
-            new TechniciansDesc(160, new SimpleTechnicianStrategy())  //
+        technicians = new TechniciansDesc[]{
+                new TechniciansDesc(160, new SimpleTechnicianStrategy())  //
         };
 
         Cl = 20;
-        clients = new ClientsDesc[] {
-            new ClientsDesc(20, new UniformClientStrategy())  //
+        clients = new ClientsDesc[]{
+                new ClientsDesc(20, new UniformClientStrategy())  //
         };
 
         S = 4;
-        stations = new StationsDesc[] {
-            new StationsDesc(4, 40, 5)  //
+        stations = new StationsDesc[]{
+                new StationsDesc(4, 40, 5)  //
         };
 
         Co = 5;
-        companies = new CompaniesDesc[] {
-            new CompaniesDesc(5, new SimpleCompanyStrategy(), 32)  //
+        companies = new CompaniesDesc[]{
+                new CompaniesDesc(3, new SimpleCompanyStrategy(), 32),
+                new CompaniesDesc(2, new UmNomeParaMeter(), 32)
         };
 
         assertValid();
